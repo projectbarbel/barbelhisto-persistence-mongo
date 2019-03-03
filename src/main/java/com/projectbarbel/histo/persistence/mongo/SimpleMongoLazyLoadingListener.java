284f679b11c2f3ec7bac2c1ce2cba2b31ec1fd27
Niklas Schlimm <ns103@hotmail.de>
2019-03-03 20:50:18 +0100

    AbstractListeners implemented from core package

--- src/main/java/com/projectbarbel/histo/persistence/mongo/SimpleMongoLazyLoadingListener.java
+++ src/main/java/com/projectbarbel/histo/persistence/mongo/SimpleMongoLazyLoadingListener.java
@@ -2,73 +2,49 @@ package com.projectbarbel.histo.persistence.mongo;
 
 import static com.mongodb.client.model.Filters.eq;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 import org.bson.Document;
-import org.projectbarbel.histo.BarbelHisto;
-import org.projectbarbel.histo.BarbelHistoCore;
-import org.projectbarbel.histo.BarbelMode;
-import org.projectbarbel.histo.BarbelQueries;
-import org.projectbarbel.histo.DocumentJournal;
-import org.projectbarbel.histo.event.EventType.BarbelInitializedEvent;
-import org.projectbarbel.histo.event.EventType.InitializeJournalEvent;
-import org.projectbarbel.histo.event.EventType.RetrieveDataEvent;
+import org.projectbarbel.histo.extension.AbstractLazyLoadingListener;
 import org.projectbarbel.histo.model.Bitemporal;
 import org.projectbarbel.histo.model.BitemporalVersion;
 
-import com.google.common.eventbus.Subscribe;
 import com.google.gson.Gson;
-import com.googlecode.cqengine.query.Query;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 
-/**
- * Mongo shadow lazy loading listener implementation to pre-fetch saved data
- * from previous sessions back to {@link BarbelHisto}. <br>
- * 
- * The simple listener implementations provide support for all
- * {@link BarbelQueries} and all custom queries as long they use the
- * {@link BarbelQueries#DOCUMENT_ID} as a filter criterion.
- * 
- * If you run an instance of {@link BarbelHisto} as global singleton in your
- * application set-up, set the <code>singletonContext</code> flag to true. This
- * will increase performance as data is the listener refuses to refresh data on
- * each retrieve operation. This is safe, if only ever one {@link BarbelHisto}
- * instance is using the {@link MongoCollection}.
- * 
- * @author Niklas Schlimm
- *
- */
-public final class SimpleMongoLazyLoadingListener {
+public class SimpleMongoLazyLoadingListener extends AbstractLazyLoadingListener<MongoCollection<Document>, Document> {
 
-    private MongoCollection<Document> shadow;
     private final MongoClient client;
     private final String dbName;
     private final String collectionName;
-    private final Class<?> managedType;
-    private final Class<?> persistedType;
-    private final BarbelMode mode;
-    private final String documentIdFieldName;
-    private final Gson gson;
-    private final boolean singletonContext;
-
-    private SimpleMongoLazyLoadingListener(MongoClient client, String dbName, String collectionName,
-            Class<?> managedType, Gson gson, boolean singletonContext) {
+    
+    public SimpleMongoLazyLoadingListener(MongoClient client, String dbName, String collectionName, Class<?> managedType, Gson gson,
+            boolean singletonContext) {
+        super(managedType, gson, singletonContext);
         this.client = client;
         this.dbName = dbName;
         this.collectionName = collectionName;
-        this.managedType = managedType;
-        this.gson = gson;
-        this.singletonContext = singletonContext;
-        if (Bitemporal.class.isAssignableFrom(managedType))
-            mode = BarbelMode.BITEMPORAL;
-        else
-            mode = BarbelMode.POJO;
-        this.documentIdFieldName = mode.getDocumentIdFieldNameOnPersistedType(managedType);
-        this.persistedType = mode.getPersistenceObjectType(managedType);
+    }
+
+    public Iterable<Document> queryAll() {
+        return shadow.find();
+    }
+
+    public Iterable<Document> queryJournal(Object id) {
+        return shadow.find(eq(documentIdFieldName, id));
+    }
+
+    public MongoCollection<Document> createResource() {
+        return client.getDatabase(dbName).getCollection(collectionName);
+    }
+
+    public Bitemporal toPersistedType(Document document) {
+        String json = document.toJson();
+        Object object = gson.fromJson(json, persistedType);
+        if (object instanceof BitemporalVersion) {
+            BitemporalVersion bv = (BitemporalVersion) object;
+            bv.setObject(gson.fromJson(gson.toJsonTree(bv.getObject()).toString(), managedType));
+        }
+        return (Bitemporal)object;
     }
 
     public static SimpleMongoLazyLoadingListener create(MongoClient client, String dbName, String collectionName,
@@ -79,72 +55,6 @@ public final class SimpleMongoLazyLoadingListener {
     public static SimpleMongoLazyLoadingListener create(MongoClient client, String dbName, String collectionName,
             Class<?> managedType, Gson gson) {
         return new SimpleMongoLazyLoadingListener(client, dbName, collectionName, managedType, gson, false);
-    }
-
-    @Subscribe
-    public void handleInitialization(BarbelInitializedEvent event) {
-        try {
-            shadow = client.getDatabase(dbName).getCollection(collectionName);
-        } catch (Exception e) {
-            event.failed(e);
-        }
-    }
-
-    @Subscribe
-    public void handleRetrieveData(RetrieveDataEvent event) {
-        try {
-            Query<?> query = (Query<?>) event.getEventContext().get(RetrieveDataEvent.QUERY);
-            BarbelHistoCore<?> histo = (BarbelHistoCore<?>) event.getEventContext().get(RetrieveDataEvent.BARBEL);
-            final List<Object> ids = BarbelQueries.returnIDsForQuery(query, new ArrayList<>());
-            if (!ids.isEmpty()) {
-                for (Object id : ids) {
-                    if (!histo.contains(id) || (histo.contains(id) && !singletonContext)) {
-                        List<Bitemporal> docs = StreamSupport
-                                .stream(shadow.find(eq(documentIdFieldName, id)).spliterator(), true)
-                                .map(d -> (Bitemporal) toPersistedType((Document) d)).collect(Collectors.toList());
-                        if (histo.contains(id))
-                            ((BarbelHistoCore<?>)histo).unloadQuiet(id);
-                        ((BarbelHistoCore<?>)histo).loadQuiet(docs);
-                    }
-                }
-            } else {
-                // literally the complete refresh with backbone data
-                List<Bitemporal> docs = StreamSupport.stream(shadow.find().spliterator(), true)
-                        .map(d -> (Bitemporal) toPersistedType((Document) d)).collect(Collectors.toList());
-                histo.getContext().getBackbone().clear();
-                ((BarbelHistoCore<?>)histo).loadQuiet(docs);
-            }
-        } catch (Exception e) {
-            event.failed(e);
-        }
-    }
-
-    private Object toPersistedType(Document document) {
-        String json = document.toJson();
-        Object object = gson.fromJson(json, persistedType);
-        if (object instanceof BitemporalVersion) {
-            BitemporalVersion bv = (BitemporalVersion) object;
-            bv.setObject(gson.fromJson(gson.toJsonTree(bv.getObject()).toString(), managedType));
-        }
-        return object;
-    }
-
-    @Subscribe
-    public void handleInitializeJournal(InitializeJournalEvent event) {
-        try {
-            DocumentJournal journal = (DocumentJournal) event.getEventContext().get(DocumentJournal.class);
-            BarbelHisto<?> histo = (BarbelHisto<?>) event.getEventContext().get(RetrieveDataEvent.BARBEL);
-            if (!histo.contains(journal.getId()) || (histo.contains(journal.getId()) && !singletonContext)) {
-                List<Bitemporal> docs = StreamSupport
-                        .stream(shadow.find(eq(documentIdFieldName, journal.getId())).spliterator(), true)
-                        .map(d -> (Bitemporal) toPersistedType((Document) d)).collect(Collectors.toList());
-                if (histo.contains(journal.getId()))
-                    ((BarbelHistoCore<?>)histo).unloadQuiet(journal.getId());
-                ((BarbelHistoCore<?>)histo).loadQuiet(docs);
-            }
-        } catch (Exception e) {
-            event.failed(e);
-        }
     }
 
 }
--- src/main/java/com/projectbarbel/histo/persistence/mongo/SimpleMongoUpdateListener.java
+++ src/main/java/com/projectbarbel/histo/persistence/mongo/SimpleMongoUpdateListener.java
@@ -2,181 +2,69 @@ package com.projectbarbel.histo.persistence.mongo;
 
 import static com.mongodb.client.model.Filters.eq;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-import org.apache.commons.lang3.Validate;
 import org.bson.Document;
-import org.projectbarbel.histo.BarbelHisto;
-import org.projectbarbel.histo.BarbelHistoCore;
-import org.projectbarbel.histo.BarbelMode;
-import org.projectbarbel.histo.DocumentJournal.Inactivation;
-import org.projectbarbel.histo.event.EventType.BarbelInitializedEvent;
-import org.projectbarbel.histo.event.EventType.OnLoadOperationEvent;
-import org.projectbarbel.histo.event.EventType.UnLoadOperationEvent;
-import org.projectbarbel.histo.event.EventType.UpdateFinishedEvent;
-import org.projectbarbel.histo.model.Bitemporal;
-import org.projectbarbel.histo.model.BitemporalStamp;
-import org.projectbarbel.histo.model.BitemporalVersion;
+import org.projectbarbel.histo.extension.AbstractMongoUpdateListener;
 
-import com.google.common.eventbus.Subscribe;
 import com.google.gson.Gson;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.result.DeleteResult;
 
-/**
- * Mongo shadow update listener implementation to synchronize
- * {@link BarbelHisto} backbone updates to {@link MongoCollection}.
- * 
- * @author Niklas Schlimm
- *
- */
-public final class SimpleMongoUpdateListener {
-
-    private static final String VERSION_ID = ".versionId";
-    private MongoCollection<Document> shadow;
+public class SimpleMongoUpdateListener extends AbstractMongoUpdateListener<MongoCollection<Document>, Document>{
     private final MongoClient client;
     private final String dbName;
     private final String collectionName;
-    private final BarbelMode mode;
-    private String versionIdFieldName;
-    private final Gson gson;
-    private final Class<?> managedType;
-    private final String documentIdFieldName;
-    private final Class<?> persistedType;
 
-    public SimpleMongoUpdateListener(MongoClient client, String dbName, String collectionName, Class<?> managedType,
-            Gson gson) {
+    public SimpleMongoUpdateListener(Class<?> managedType, Gson gson, MongoClient client, String dbName,
+            String collectionName) {
+        super(managedType, gson);
         this.client = client;
         this.dbName = dbName;
         this.collectionName = collectionName;
-        this.managedType = managedType;
-        this.gson = gson;
-        if (Bitemporal.class.isAssignableFrom(managedType)) {
-            mode = BarbelMode.BITEMPORAL;
-        } else {
-            mode = BarbelMode.POJO;
-        }
-        this.versionIdFieldName = mode.getStampFieldName(mode.getPersistenceObjectType(managedType),
-                BitemporalStamp.class) + VERSION_ID;
-        this.documentIdFieldName = mode.getDocumentIdFieldNameOnPersistedType(managedType);
-        this.persistedType = mode.getPersistenceObjectType(managedType);
+    }
+
+
+    @Override
+    public MongoCollection<Document> createResource() {
+        return client.getDatabase(dbName).getCollection(collectionName);
+    }
+
+
+    @Override
+    public long delete(String versionId) {
+        return shadow
+                .deleteOne(eq(versionIdFieldName, versionId)).getDeletedCount();
+    }
+
+    @Override
+    public long deleteJournal(Object id) {
+        return shadow.deleteMany(eq(documentIdFieldName, id)).getDeletedCount();
     }
 
     public static SimpleMongoUpdateListener create(MongoClient client, String dbName, String collectionName,
             Class<?> managedType, Gson gson) {
-        return new SimpleMongoUpdateListener(client, dbName, collectionName, managedType, gson);
+        return new SimpleMongoUpdateListener(managedType, gson, client, dbName, collectionName);
     }
 
-    @Subscribe
-    public void handleInitialization(BarbelInitializedEvent event) {
-        try {
-            shadow = client.getDatabase(dbName).getCollection(collectionName);
-        } catch (Exception e) {
-            event.failed(e);
-        }
+    @Override
+    public void insertDocuments(List<Document> documentsToInsert) {
+        shadow.insertMany(documentsToInsert);
     }
 
-    @Subscribe
-    public void handleLoadOperation(OnLoadOperationEvent event) {
-        try {
-            @SuppressWarnings("unchecked")
-            Collection<Bitemporal> managedBitemporalsToLoad = (Collection<Bitemporal>) event.getEventContext()
-                    .get(OnLoadOperationEvent.DATA);
-            if (managedBitemporalsToLoad.size()==0)
-                return;
-            for (Bitemporal bitemporal : managedBitemporalsToLoad) {
-                if (shadow.find(eq(documentIdFieldName, bitemporal.getBitemporalStamp().getDocumentId())).iterator()
-                        .hasNext())
-                    throw new IllegalStateException("document with id exists: "
-                            + bitemporal.getBitemporalStamp().getDocumentId() + " - unable to load");
-            }
-            // @formatter:off
-            List<Document> documentsToInsert = managedBitemporalsToLoad.stream()
-                    .map(mode::managedBitemporalToCustomPersistenceObject) // to persistence object
-                    .map(gson::toJson) // to json
-                    .map(Document::parse) // to mongo Document
-                    .collect(Collectors.toList()); // to list
-            // @formatter:on
-            shadow.insertMany(documentsToInsert);
-        } catch (Exception e) {
-            event.failed(e);
-        }
+    @Override
+    public FindIterable<Document> queryJournal(Object id) {
+        return shadow.find(eq(documentIdFieldName, id));
+    }
+    @Override
+    public Document fromJsonToStoredDocument(String json) {
+        return Document.parse(json);
     }
 
-    @Subscribe
-    public void handleUnLoadOperation(UnLoadOperationEvent event) {
-        try {
-            BarbelHisto<?> histo = (BarbelHisto<?>) event.getEventContext().get(UnLoadOperationEvent.BARBEL);
-            Object[] documentIds = (Object[]) event.getEventContext().get(UnLoadOperationEvent.DOCUMENT_IDs);
-            for (Object id : documentIds) {
-                List<Bitemporal> docs = StreamSupport
-                        .stream(shadow.find(eq(documentIdFieldName, id)).spliterator(), true)
-                        .map(d -> (Bitemporal) toPersistedType((Document) d)).collect(Collectors.toList());
-                if (histo.contains(id))
-                    ((BarbelHistoCore<?>) histo).unloadQuiet(id);
-                ((BarbelHistoCore<?>) histo).loadQuiet(docs);
-                shadow.deleteMany(eq(documentIdFieldName, id));
-            }
-        } catch (Exception e) {
-            event.failed(e);
-        }
-    }
-
-    private Object toPersistedType(Document document) {
-        String json = document.toJson();
-        Object object = gson.fromJson(json, persistedType);
-        if (object instanceof BitemporalVersion) {
-            BitemporalVersion bv = (BitemporalVersion) object;
-            bv.setObject(gson.fromJson(gson.toJsonTree(bv.getObject()).toString(), managedType));
-        }
-        return object;
-    }
-
-    @Subscribe
-    public void handleUpdate(UpdateFinishedEvent event) {
-        try {
-            @SuppressWarnings("unchecked")
-            List<Bitemporal> managedBitemporalsInserted = (List<Bitemporal>) event.getEventContext()
-                    .get(UpdateFinishedEvent.NEWVERSIONS);
-            @SuppressWarnings("unchecked")
-            Set<Inactivation> inactivations = (Set<Inactivation>) event.getEventContext()
-                    .get(UpdateFinishedEvent.INACTIVATIONS);
-            // delete first ! cause version id is the key for deletion, and replaced new
-            // objects carry same version IDs
-            List<Bitemporal> objectsRemoved = inactivations.stream()
-                    .map(r -> mode.managedBitemporalToCustomPersistenceObject(r.getObjectRemoved()))
-                    .collect(Collectors.toList());
-            List<DeleteResult> results = objectsRemoved.stream()
-                    .map(objectToRemove -> (DeleteResult) delete(objectToRemove)).collect(Collectors.toList());
-            Validate.validState(results.stream().filter(r -> r.getDeletedCount() != 1).count() == 0,
-                    "delete operation failed - delete count must always be = 1");
-            // // @formatter:off
-            List<Document> documentsToInsert = managedBitemporalsInserted.stream()
-                    .map(mode::managedBitemporalToCustomPersistenceObject) // to persistence object
-                    .map(gson::toJson) // to json
-                    .map(Document::parse) // to mongo Document
-                    .collect(Collectors.toList()); // to list
-            List<Document> documentsAddedOnReplacements = inactivations.stream()
-                    .map(r -> mode.managedBitemporalToCustomPersistenceObject(r.getObjectAdded())) // to persistence objects
-                    .map(gson::toJson) // to json
-                    .map(Document::parse) // to mongo Document
-                    .collect(Collectors.toList()); // to list
-            documentsToInsert.addAll(documentsAddedOnReplacements);
-            // @formatter:on
-            shadow.insertMany(documentsToInsert);
-        } catch (Exception e) {
-            event.failed(e);
-        }
-    }
-
-    private DeleteResult delete(Object objectToRemove) {
-        return shadow
-                .deleteOne(eq(versionIdFieldName, ((Bitemporal) objectToRemove).getBitemporalStamp().getVersionId()));
+    @Override
+    public String fromStroredDocumentToJson(Document document) {
+        return document.toJson();
     }
 
 }
